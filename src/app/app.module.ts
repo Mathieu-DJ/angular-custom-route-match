d34a629dbea7606daffd431a4a60fefac7f088e3
Mathieu <null>
2021-12-20 12:58:48 +0100

    ne fonctionne pas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: 'p/:username',
+    matcher: (url) => {
+      if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+        return {
+          consumed: url,
+          posParams: {
+            username: new UrlSegment(url[0].path.substr(1), {})
+          }
+        };
+      }
+    
+      return null;
+    },
+    component: ProfileComponent
+  }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
